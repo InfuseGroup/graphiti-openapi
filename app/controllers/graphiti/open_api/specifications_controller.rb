292166fe3705a2bfb37af1a21a365910f473c014
Tom Luce <thomasgluce@gmail.com>
2021-02-17 11:51:13 +0000

    build: I have no words

--- app/controllers/graphiti/open_api/specifications_controller.rb
+++ app/controllers/graphiti/open_api/specifications_controller.rb
@@ -1,19 +1,21 @@
-module Graphiti::OpenAPI
-  class SpecificationsController < ::ApplicationController
-    def index
-      respond_to do |format|
-        format.html
-        format.json { render json: generator.to_openapi }
-        format.yaml { render text: generator.to_openapi(format: :yaml), layout: nil }
+module Graphiti
+  module OpenAPI
+    class SpecificationsController < ::ApplicationController
+      def index
+        respond_to do |format|
+          format.html
+          format.json { render json: generator.to_openapi }
+          format.yaml { render text: generator.to_openapi(format: :yaml), layout: nil }
+        end
       end
+
+      private
+
+      def generator
+        @generator ||= Generator.new
+      end
+
+      helper_method :generator
     end
-
-    private
-
-    def generator
-      @generator ||= Generator.new
-    end
-
-    helper_method :generator
   end
 end
